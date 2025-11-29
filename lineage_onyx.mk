2f0ff2669cede1ef80807341ffb7e71b78a41015
marcoodhb <chahuaram13@gmail.com>
2025-11-29 15:08:18 -0500

    Update lineage_onyx.mk

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit from onyx device
 $(call inherit-product, device/xiaomi/onyx/device.mk)
